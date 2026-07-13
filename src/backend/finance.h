fa41070c6820d9c0ee25c28b04defd4e2aeb7f05
Md Shifat Reza <tomar-github-email@example.com>
2026-07-13 17:02:26 +0600

    updated finance.h

--- src/backend/finance.h
+++ src/backend/finance.h
@@ -2,16 +2,31 @@
 #define FINANCE_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-//category breakdown
-typedef struct {
-    char category_name[100];
-    double amount;
-    double percentage;
-} CategoryBreakdown;
+    // category breakdown
+    typedef struct
+    {
+        char category_name[100];
+        double amount;
+        double percentage;
+    } CategoryBreakdown;
+    // total balance calculation
+    double calculate_total_balance();
 
+    // monthly income
+    double calculate_monthly_income(const char *month_yyyy_mm);
+
+    // monthly expense
+    double calculate_monthly_expense(const char *month_yyyy_mm);
+
+    // cash flow
+    double calculate_net_cash_flow(const char *month_yyyy_mm);
+
+    // expense on different category
+    int get_category_spending_breakdown(const char *month_yyyy_mm, CategoryBreakdown *out_breakdown, int max_categories);
 #ifdef __cplusplus
 }
 #endif
