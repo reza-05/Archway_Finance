41f9e628d1a0773eb0857b60efae1c954985bf84
Mustakim <mohammedmustakim05@gmail.com>
2026-07-14 22:19:09 +0600

    added load data from file fucntion

---
+++ database/database.c
@@ -0,0 +1,62 @@
+#include "stdio.h"
+#include "database.h"
+#include "string.h"
+
+void init_database(Database *db) {     // intialized count to 0
+    if (db != NULL) {
+        db->count = 0;
+    }
+}
+
+
+//injection of values into database
+int add_transaction(Database *db, double amount, int is_income, const char *category, const char *date, const char *description) {
+    Transaction *t = &db->list[db->count];
+
+    t->id = db->count+1;
+
+    t->amount= amount;
+
+    t->is_income = is_income;
+
+    strcpy(t->category, category);
+    t->category[49]= '\0';
+
+    strcpy(t->date, date);
+    t->date[19]= '\0';
+
+    strcpy(t->description,description);
+    t->description[49] = '\0';
+
+    db->count++;
+
+}
+
+int save_database(const Database *db, const char *filename){
+
+
+    FILE *file = fopen(filename, "wb");
+
+    fwrite(&(db->count), sizeof(int), 1, file);  // transaction count first
+
+    if(db->count > 0)
+        fwrite(db->list,sizeof(Transaction),db->count,file);   // then copy all transactions
+
+    fclose(file);
+}
+
+
+
+int load_database(Database *db, const char *filename){
+
+    FILE *file = fopen(filename, "rb");
+
+  if(file == NULL)
+        printf("Please input the correct file name.");
+
+    fread(&(db->count), sizeof(int), 1, file); // get count
+
+    fread(db->list, sizeof(Transaction),db->count,file); // load transactions
+
+    fclose(file);
+}
