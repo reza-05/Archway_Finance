7544a5554b3ec05a9418b65055c09ed7620bf7bf
Mustakim <mohammedmustakim05@gmail.com>
2026-07-14 21:23:38 +0600

    Added the helper functions of adding transactions

---
+++ database/database.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "database.h"
+#include "string.h"
+
+void init_database(Database *db) {     // intialized count to 0
+    if (db != NULL) {
+        db->count = 0;
+    }
+}
+
+
+//injection of values into database
+int add_transaction(Database *db, double amount, int is_income, const char *category, const char *date, const char *description) {
+    Transaction *t = &db->list[db->count];
+
+    t->id = db->count+1;
+
+    t->amount= amount;
+
+    t->is_income = is_income;
+
+    strcpy(t->category, category);
+    t->category[49]= '\0';
+
+    strcpy(t->date, date);
+    t->date[19]= '\0';
+
+    strcpy(t->description,description);
+    t->description[49] = '\0';
+
+    db->count++;
+
+}
