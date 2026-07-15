9b82098f55f50361c533d4795e0c05dba42872de
Md Shifat Reza <tomar-github-email@example.com>
2026-07-15 08:29:53 +0600

    finance.h final update

--- src/backend/finance.h
+++ src/backend/finance.h
@@ -1,12 +1,23 @@
 #ifndef FINANCE_H
 #define FINANCE_H
 
-//database.h linking
+// database.h linking
 #include "../../database/database.h"
 
- // total balance calculation
+// total balance calculation
 
 double calculate_total_balance(const Database *db);
 
+// total income
+
+double calculate_monthly_income(const Database *db, const char *month_yyyy_mm);
+
+// monthly expense
+
+double calculate_monthly_expense(const Database *db, const char *month_yyyy_mm);
+
+// cash flow
+
+double calculate_net_cash_flow(const Database *db, const char *month_yyyy_mm);
 
 #endif // FINANCE_H
