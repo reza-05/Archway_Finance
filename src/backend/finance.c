2af07bc121aac3e49cadd717931d5ef0d0addcd2
Md Shifat Reza <tomar-github-email@example.com>
2026-07-15 08:40:00 +0600

    added total income function in finance.c

--- src/backend/finance.c
+++ src/backend/finance.c
@@ -1,16 +1,39 @@
 #include "finance.h"
 #include <string.h>
 
-//total balance till now
-double calculate_total_balance(const Database *db) {
-    if (db == NULL) return 0.0;
-    
+// total balance till now
+double calculate_total_balance(const Database *db)
+{
+    if (db == NULL)
+        return 0.0;
+
     double total = 0.0;
-    for (int i = 0; i < db->count; i++) {
-        if (db->list[i].is_income == 1) {
+    for (int i = 0; i < db->count; i++)
+    {
+        if (db->list[i].is_income == 1)
+        {
             total += db->list[i].amount; // add income
-        } else {
+        }
+        else
+        {
             total -= db->list[i].amount; // minus expense
+        }
+    }
+    return total;
+}
+// total income of a specific month
+double calculate_monthly_income(const Database *db, const char *month_yyyy_mm)
+{
+    if (db == NULL || month_yyyy_mm == NULL)
+        return 0.0;
+
+    double total = 0.0;
+    for (int i = 0; i < db->count; i++)
+    {
+        // add income if date matches
+        if (db->list[i].is_income == 1 && strncmp(db->list[i].date, month_yyyy_mm, 7) == 0)
+        {
+            total += db->list[i].amount;
         }
     }
     return total;
