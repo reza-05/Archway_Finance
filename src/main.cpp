c47650a27d1b494b1e89a506af8b881da2f03ec6
Md Shifat Reza <tomar-github-email@example.com>
2026-07-15 08:25:31 +0600

    cmakelists updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <sqlite3.h>
+using namespace std;
 
-int main() {
-    std::cout << "Welcome to Archway - Personal Finance Manager!" << std::endl;
-    std::cout << "SQLite3 Version: " << sqlite3_libversion() << std::endl;
+int main()
+{
+    cout << "Welcome to Archway - Personal Finance Manager!" << endl;
+    cout << "SQLite3 Version: " << sqlite3_libversion() << endl;
     return 0;
 }
